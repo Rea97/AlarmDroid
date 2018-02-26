7675a491f9fafd95cd285253dd8b02592d2201d9
Oziel Martinez <ozielrea97@hotmail.com>
2018-02-25 22:02:06 -0600

    Create dummy login

--- app/src/main/java/me/readeveloper/alarmdroid/LoginScreenActivity.java
+++ app/src/main/java/me/readeveloper/alarmdroid/LoginScreenActivity.java
@@ -1,13 +1,32 @@
 package me.readeveloper.alarmdroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginScreenActivity extends AppCompatActivity {
+
+    private String defaultEmail = "oziel@test.com";
+    private String defaultPassword = "secret";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+    }
+
+    public void login(View view) {
+        EditText emailText = findViewById(R.id.emailText);
+        EditText passwordText = findViewById(R.id.passwordText);
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (email.equals(this.defaultEmail) && password.equals(this.defaultPassword)) {
+            // redirect to another activity
+        }
+        Toast.makeText(this, "Credenciales inválidas.", Toast.LENGTH_LONG).show();
     }
 }
