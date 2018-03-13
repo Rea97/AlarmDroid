76432dabc294211cbe244d06086628b68870af90
Rated223 <Rated223@outlook.com>
2018-03-12 22:12:43 -0600

    Add the modifications for the responsive list of AlertsListActivity Create the classes AdapterAllAlerts and AlertItem Create the layout for the format of a list item Modified the AlertsListActivity whit a recyclerView add the icon of the search button

---
+++ app/src/main/java/me/readeveloper/alarmdroid/AlertItem.java
@@ -0,0 +1,51 @@
+package me.readeveloper.alarmdroid;
+
+/**
+ * Created by HP Gamer on 08/03/2018.
+ */
+
+public class AlertItem {
+    private String tipo;
+    private String mensaje;
+    private String zona;
+    private String fecha;
+
+    public AlertItem(String tipo, String mensaje, String zona, String fecha) {
+        this.tipo = tipo;
+        this.mensaje = mensaje;
+        this.zona = zona;
+        this.fecha = fecha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+}
