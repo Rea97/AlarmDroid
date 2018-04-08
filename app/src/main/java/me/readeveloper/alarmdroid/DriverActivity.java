6e957307b49855b37deb0d9913835caac956b36a
Oziel Martínez <ozielrea97@hotmail.com>
2018-04-07 21:12:34 -0500

    Add action to control buttons on DriverActivity
    Now each button sends a status to the web api and changes the robot status
    A new button Stop was added

--- app/src/main/java/me/readeveloper/alarmdroid/DriverActivity.java
+++ app/src/main/java/me/readeveloper/alarmdroid/DriverActivity.java
@@ -2,11 +2,17 @@ package me.readeveloper.alarmdroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
 import me.readeveloper.alarmdroid.utils.Auth;
+import me.readeveloper.alarmdroid.utils.HttpClient;
 
 public class DriverActivity extends AppCompatActivity {
 
@@ -45,5 +51,55 @@ public class DriverActivity extends AppCompatActivity {
             Toast.makeText(this, "Opcion no valida", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setStatus(View view) {
+        String status;
+        switch (view.getId()) {
+            case R.id.ControlAvanzar:
+                status = "adelante";
+                break;
+            case R.id.ControlAtras:
+                status = "atras";
+                break;
+            case R.id.ControlDer:
+                status = "derecha";
+                break;
+            case R.id.ControlIzq:
+                status = "izquierda";
+                break;
+            case R.id.ControlDetener:
+                status = "detenido";
+                break;
+            default:
+                status = "detenido";
+                break;
+        }
+
+        this.sendStatus(status);
+    }
+
+    private void sendStatus(String status) {
+        HttpClient http = new HttpClient("https://alarmdroid.herokuapp.com/api/users/robots/status", this);
+        http.setHeader("Authorization", "Bearer " + Auth.getApiTokenFromSharedPreferences(this))
+                .setHeader("Content-Type", "application/x-www-form-urlencoded")
+                .setHeader("Accept", "application/json")
+                .setParameter("status", status);
+
+        http.put(new Response.Listener() {
+            @Override
+            public void onResponse(Object response) {
+                Toast.makeText(
+                        DriverActivity.this,
+                        "Estatus del robot ha cambiado.",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("ErrorResponse", "Error on request.", error);
+            }
+        });
     }
 }
